d0d55c6ef7e148f6ffa1fffcd1ce69b29a7ba4d0
Lui Villarias <luivillarias@gmail.com>
2015-09-11 19:46:35 +1200

    issue #10, #12 complete

--- SCPuploadADXL.py
+++ SCPuploadADXL.py
@@ -8,7 +8,6 @@ import os
 from time import *
 from datetime import datetime
 
-
 # glob used for easy file searching in directory
 from glob import glob
 
@@ -16,67 +15,84 @@ from glob import glob
 import plumbum
 
 
-uploadQueue = deque()
-
-#dataFileNameList = []
 dataSentHistoryList = []
-
+uploadQueue = deque()
 dataFolder = "ADXLData/"
 piID = "RPi_Lui"
+logList = []
 
 interval = 0.5 # in minutes
 
-SCPSession = 0
+def log(description, save = True, printScreen = True):
+	if (save):
+		logList.append((str(datetime.now()) + " - ") + description)
+	
+	if (printScreen):
+		print(description)
+	return True
+
+
+def writeLog():
+	fileName = dataFolder + piID + "_upload_log"
+	logFile = open(fileName, 'a')
+	
+	for line in logList:
+		logFile.write(line + "\n")
+		
+	logFile.write("\n")
+	
+	logFile.close()
+	return True
+
 
 def scanFolder(dataFolder):
-	global dataSentHistoryList
 	uploadQueue = deque()
 	
 	dataFileNameList = glob(dataFolder + piID + "_data*")
-	print ("\nFiles found: " + str(len(dataFileNameList)) + "\n")
+	log("New files found: " + str(len(dataFileNameList)))
 	
 	print("Populating queue...")	
 	for dataFileName in dataFileNameList:
 		if ((dataFileName in dataSentHistoryList) == False):
 			uploadQueue.append(dataFileName)
-			print(dataFileName + " added to upload queue")
+			log(dataFileName + " added to upload queue")
 	return uploadQueue
 	
 def addToSentHistoryList(dataFileName):
-	global dataSentHistoryList
 	dataSentHistoryList.append(dataFileName)
 
 def connectToSCPHost(username, remote):
-	print("Connecting to SCP Session - " + username + "@" + remote)
+	sshLocation = "/home/pi/.ssh/DOPrivateKeyOSSH"
+	
+	log("Connecting to SCP Session - " + username + "@" + remote)
 	try:
-		SCPSession = plumbum.machines.SshMachine(remote, user=username, keyfile="/home/pi/.ssh/DOPrivateKeyOSSH")
-		print("CONNECTED to SCP Session\n")
+		SCPSession = plumbum.machines.SshMachine(remote, user=username, keyfile=sshLocation)
+		log("Connected to SCP Session")
 		return SCPSession
 	except:
-		print("!!! ERROR - Unable to connect to SCP Session !!!")
+		log("!!! - ERROR - connectToSCPHost() - failed to connect")
 		return -1
 	
 
 def disconnectFromSCPHost(SCPSession):
-	SCPSession.close()
+	return SCPSession.close()
 	
 def uploadFile(fileName, SCPSession, location = "~/sftp"):
-	print("Uploading " + fileName)
+	log("Uploading " + fileName)
 	try:
 		localFile = plumbum.local.path(fileName)
 		remoteDestination = SCPSession.path(location)
 		plumbum.path.utils.copy(localFile, remoteDestination)
 	except:
-		print("!!! ERROR - Failure uploading file")
+		log("ERROR - Failure uploading file")
 	
 	addToSentHistoryList(fileName)
 	
 def uploadTheQueue(uploadQueue, SCPSession):
-	print("Uploading [" + str(len(uploadQueue)) + "] data files in queue...")
+	# Upload entire queue
+	log("Uploading [" + str(len(uploadQueue)) + "] data files in queue...")
 	while uploadQueue:
 		uploadFile(uploadQueue.popleft(), SCPSession)
-		
-	uploadFile("ADXLData/RPi_Lui_log", SCPSession)
 
 def clearScreen():
 	os.system('clear')		
@@ -85,30 +101,39 @@ def clearScreen():
 #### MAIN PROGRAM STARTS HERE
 clearScreen()
 
-# convert interval to seconds
-interval = float(input("How often to check and upload (in minutes): "))
-interval = interval * 60
+# Ask how often we should try to upload
+interval = float(input("How often to check and upload (in minutes): ") * 60)
 		
 # First scan the folder for files and grab files
 while True:
 	clearScreen()
 
-	print ("\n######### uploadADXL running #########\n")
+	print ("\n######### uploadADXL (SCP) running #########\n")
 	
 	# Scan folder for new files to send out
 	print("Scanning folder and populating upload queue...")
 	uploadQueue = scanFolder(dataFolder)
 	
+	SCPSession = connectToSCPHost("root", "114.236.141.183")
+	# "104.236.141.183"
 	
-	# If queue has contents, upload them
-	if (uploadQueue):
-		print("\nUploading new files in queue...")
-		SCPSession = connectToSCPHost("root", "104.236.141.183")
-		if (SCPSession != -1):
+	# Only attempt any upload if connection was established
+	if (SCPSession != -1):
+		# If queue has contents, upload them
+		if (uploadQueue):
+			print("\nUploading new files in queue...")
+			# Upload the queue of data files and then upload data log
 			uploadTheQueue(uploadQueue, SCPSession)
-			disconnectFromSCPHost(SCPSession)
-	else:
-		print("\nQueue empty!")
+				
+		else:
+			print("\nQueue empty!")
+
+		writeLog()
+		
+		# Upload data log file (to update)
+		uploadFile("ADXLData/RPi_Lui_log", SCPSession)
+		uploadFile("ADXLData/RPi_Lui_upload_log", SCPSession)
+		SCPSession = disconnectFromSCPHost(SCPSession)
 
 	print("\n\nSleeping until next upload interval...")
 	print("Slept @ " + str(datetime.now()))
